46c4304bdf552d279f8858af85c7dde88889cb32
Renato Santos de Souza <ssrenatoss@hotmail.com>
2019-05-25 15:28:16 -0300

    Correção entidade Produto }

---
+++ QuickBuy.Dominio/Enumerados/TipoFormaPagamentoEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace QuickBuy.Dominio.Enumerados
+{
+    public enum TipoFormaPagamentoEnum
+    {
+        NaoDefinido = 0,
+        Boleto = 1,
+        CartaoCredito = 2,
+        Deposito = 3
+
+
+
+    }
+}
