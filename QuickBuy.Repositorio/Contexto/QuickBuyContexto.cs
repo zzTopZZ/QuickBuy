17c1124a7092a05c5131dc6a6e9e76b005c09c28
Renato Santos de Souza <ssrenatoss@hotmail.com>
2020-08-17 20:43:54 -0300

    Criação Forma Pagamento

---
+++ QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
@@ -0,0 +1,32 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio.Entidades;
+using QuickBuy.Dominio.ObjetoDeValor;
+using QuickBuy.Repositorio.Config;
+
+namespace QuickBuy.Repositorio.Contexto
+{
+    public class QuickBuyContexto : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItemPedidos { get; set; }
+        public DbSet<FormaPagamento> FormaPagemento { get; set; }
+
+
+        public QuickBuyContexto(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguration());
+            modelBuilder.ApplyConfiguration(new ProdutoConfiguration());
+            modelBuilder.ApplyConfiguration(new PedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new ItemPedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new FormaPagamentoConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
